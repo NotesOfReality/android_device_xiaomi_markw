526376b7bb6d08471d6ac61726aeb66627b2c867
Chippa-a <vusal1372@gmail.com>
2018-05-14 23:24:44 +0300

    markw: charger: Use custom healthd charger_res_images
    * Graphics are taken from stock Sony LP
    
    Change-Id: Ib08f16bfe350a4149a66373f7d9b53efd1e438fa

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service
 
+# Healthd packages
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
