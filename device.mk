292d13c7d76d6f67de07f12e088a3ff2d8db1f57
NotesOfReality <michelecurcipio@gmail.com>
2018-12-28 21:39:03 +0100

    Kinda obsessive useless commit to trigger less diffs ;) , you know?

--- device.mk
+++ device.mk
@@ -123,10 +123,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
     Snap
