629f30c6a7293da31a8b9085f32a0bc0593cd01f
Micky007 <michelecurcipio@gmail.com>
2019-02-24 03:48:31 +0100

    Implement the "XiaomiDoze" package from the "freemarkw" MDroid Oreo DT

--- device.mk
+++ device.mk
@@ -535,6 +535,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini::$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+#Custom "Ambient Display" package
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+	
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/markw/markw-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
